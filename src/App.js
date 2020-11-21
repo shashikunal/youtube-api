0e3bd10dcf6a1dc12afb89d149c7cdb6d55e285d
shashi <shashi@gmail.com>
2020-11-21 19:58:51 +0530

    added all data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component, Fragment } from "react";
 import YoutubeHeader from "./Components/YoutubeHeader/Header";
+import VideoDetails from "./Components/YoutubeVideos/VideoDetails";
+import VideoLists from "./Components/YoutubeVideos/VideoLists";
 import api, { baseParams } from "./Services/Api";
 
 class App extends Component {
@@ -7,7 +9,11 @@ class App extends Component {
     super(props);
     this.state = {
       videos: [],
+      selectedVideo: null,
     };
+  }
+  componentDidMount() {
+    this.onTermSubmit("java");
   }
 
   onTermSubmit = async term => {
@@ -17,14 +23,35 @@ class App extends Component {
         ...baseParams,
       },
     });
-    console.log(response);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
+
+  onVideoSelect = async video => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <Fragment>
         <header>
           <YoutubeHeader onTermSubmit={this.onTermSubmit} />
         </header>
+        <main className="containerBlock my-3">
+          <article className="row">
+            <div className="videoPlayerBlock col-md-8">
+              <VideoDetails video={this.state.selectedVideo} />
+            </div>
+            <div className="videoListBlock col-md-4">
+              <VideoLists
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </article>
+        </main>
       </Fragment>
     );
   }
